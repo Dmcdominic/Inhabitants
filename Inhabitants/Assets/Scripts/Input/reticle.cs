81a60bce7e3c06b465b4920b8cac317687eec051
Dmcdominic <dmcdominic@gmail.com>
2019-09-17 22:18:22 -0400

    GitHub plugin, XCI plugin, basic reticle implemented.

---
+++ Inhabitants/Assets/Scripts/Input/reticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class reticle : MonoBehaviour {
+
+	// Static settings
+	public static float speed_mult = 4f;
+	public static float speed_cap = 3.5f;
+
+	// Editor fields
+	public XboxController controller;
+
+
+    // Update is called once per frame
+    void Update() {
+		Vector3 velo = new Vector2(XCI.GetAxis(XboxAxis.LeftStickX, controller), XCI.GetAxis(XboxAxis.LeftStickY, controller));
+		velo *= speed_mult;
+		if (velo.sqrMagnitude > speed_cap) {
+			velo.Normalize();
+			velo *= speed_cap;
+		}
+
+		transform.position += velo * Time.deltaTime;
+    }
+}
