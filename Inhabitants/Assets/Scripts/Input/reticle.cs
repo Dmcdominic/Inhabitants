745781cecdaeacd83368b8053c234e6f0eeda4f1
Dmcdominic <dmcdominic@gmail.com>
2019-09-26 21:54:31 -0400

    Improved reticle over_region detection

--- Inhabitants/Assets/Scripts/Input/reticle.cs
+++ Inhabitants/Assets/Scripts/Input/reticle.cs
@@ -6,8 +6,9 @@ using XboxCtrlrInput;
 public class reticle : MonoBehaviour {
 
 	// Static settings
-	public static float speed_mult = 4f;
-	public static float speed_cap = 3.5f;
+	private static float speed_mult = 4f;
+	private static float speed_cap = 3.5f;
+    private static float raycast_radius = 0.18f;
 
     // Editor fields
     public player Owner;
@@ -17,9 +18,11 @@ public class reticle : MonoBehaviour {
     // Private vars
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-    private region over_region;
 
+    private region over_region;
     private region active_region;
+    private Dictionary<region, int> touching_regions = new Dictionary<region, int>();
+    private static LayerMask region_mask;
 
 
     // Init
@@ -30,6 +33,7 @@ public class reticle : MonoBehaviour {
         line_to_active_region.startColor = player_data.colors[(int)Owner];
         line_to_active_region.endColor = Color.black;
         //line_to_active_region.
+        region_mask = LayerMask.GetMask(new string[] { "Regions" });
     }
 
     // Update is called once per frame
@@ -50,7 +54,7 @@ public class reticle : MonoBehaviour {
         }
 
         // Update over_region using raycast, rather than trigger enter/exit
-        //Physics2D.Raycast(transform.position, Vector2.up, 0.01f, )
+        update_over_region();
 
         // TESTING
         if (XCI.GetButtonDown(XboxButton.RightBumper, controller) && over_region != null) {
@@ -96,18 +100,45 @@ public class reticle : MonoBehaviour {
         }
     }
 
-    // Check when you collide with a region
-    private void OnTriggerEnter2D(Collider2D collision) {
-        region Region = collision.GetComponent<region>();
-        if (Region != null) {
-            over_region = Region;
+    private void update_over_region() {
+        RaycastHit2D[] hits = { raycast_in_dir(new Vector2( 1,  1)),
+                                raycast_in_dir(new Vector2( 1, -1)),
+                                raycast_in_dir(new Vector2(-1,  1)),
+                                raycast_in_dir(new Vector2(-1, -1)),
+                                raycast_in_dir(new Vector2( 1,  0)),
+                                raycast_in_dir(new Vector2(-1,  0)),
+                                raycast_in_dir(new Vector2( 0,  1)),
+                                raycast_in_dir(new Vector2( 0, -1)) };
+        
+        // Count the hits for each region
+        touching_regions.Clear();
+        for (int i = 0; i < hits.Length; i++) {
+            if (hits[i].collider != null) {
+                region Region = hits[i].collider.GetComponent<region>();
+                if (Region != null) {
+                    if (touching_regions.ContainsKey(Region)) {
+                        touching_regions[Region]++;
+                    } else {
+                        touching_regions[Region] = 1;
+                    }
+                } else {
+                    Debug.LogError("Object in Regions layer hit, but does not have region component");
+                }
+            }
+        }
+
+        // Determine which region has the most hits
+        over_region = (active_region != null && touching_regions.ContainsKey(active_region)) ? active_region : null;
+        int max_hits = 0;
+        foreach (region Region in touching_regions.Keys) {
+            if (Region != active_region && touching_regions[Region] > max_hits) {
+                max_hits = touching_regions[Region];
+                over_region = Region;
+            }
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        region Region = collision.GetComponent<region>();
-        if (Region != null && Region == over_region) {
-            over_region = null;
-        }
+    private RaycastHit2D raycast_in_dir(Vector2 dir) {
+        return Physics2D.Raycast(transform.position + (Vector3)dir.normalized * raycast_radius * 0.5f, dir, raycast_radius * 0.5f, region_mask);
     }
 }
