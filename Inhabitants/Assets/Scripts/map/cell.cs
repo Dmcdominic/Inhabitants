311517e26b4526c45792105f6d24afee3a01e790
bokken12 <stopwatchj@gmail.com>
2019-09-29 16:15:59 -0400

    Initial Tree System

---
+++ Inhabitants/Assets/Scripts/map/cell.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cell : MonoBehaviour
+{
+    public Sprite noTrees;
+    public Sprite withTrees;
+
+    private float _state;
+    private SpriteRenderer _rend;
+
+    public SpriteRenderer rend
+    {
+        get { return _rend; }
+        private set { _rend = value; }
+    }
+
+    public float state
+    {
+        get { return _state; }
+        set
+        {
+            _state = value;
+            if (state < 0.3)
+            {
+                rend.sprite = noTrees;
+                Vector3 scale = new Vector3(0.0f, 0.0f, 1.0f);
+                transform.localScale = scale;
+            }
+            else
+            {
+                rend.sprite = withTrees;
+                Vector3 scale = new Vector3((state - 0.3f) / 0.7f, (state - 0.3f) / 0.7f, 1.0f);
+                transform.localScale = scale;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rend = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
