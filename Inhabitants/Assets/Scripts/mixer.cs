1e946f4cff6391f2bf70b28dcea7aa4d8c61b7fa
alanzhu07 <alanzhuyixuan@gmail.com>
2019-10-18 13:12:50 -0400

    Basic sound mixer for music

---
+++ Inhabitants/Assets/Scripts/mixer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mixer : MonoBehaviour
+{
+    public AudioSource musicPlayer;
+    public AudioClip introMusic;
+    public AudioClip ecoMusic;
+    public AudioClip neuMusic;
+    public AudioClip indMusic;
+    public int ecoCutoff;
+    public int indCutoff;
+    public int value = 95;
+
+    public static void playSFX(string sfx)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.clip = introMusic;
+        musicPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!musicPlayer.isPlaying)
+        {
+            if (value < ecoCutoff)
+            {
+                musicPlayer.clip = ecoMusic;
+            }
+            else if (ecoCutoff <= value && value < indCutoff)
+            {
+                musicPlayer.clip = neuMusic;
+            }
+            else if (indCutoff <= value)
+            {
+                musicPlayer.clip = indMusic;
+            }
+            musicPlayer.Play();
+        }
+    }
+}
