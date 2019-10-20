7d8d98835ae5feb67033254fa3e7345ebfbc85f2
bokken12 <stopwatchj@gmail.com>
2019-10-20 14:43:53 -0400

    Tree Edits & Preliminary Earth Status

---
+++ Inhabitants/Assets/Scripts/map/status_controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class status_controller : MonoBehaviour
+{
+    public static status_controller instance;
+
+    public Image trees, air, temperature;
+
+    public float airSpeed = 0.01f, tempSpeed = 0.01f;
+
+    float treeLevel = 0.5f, airLevel = 0.5f, temperatureLevel = 0.5f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        treeLevel = cell_controller.instance.treeLevel();
+        airLevel = Mathf.Clamp(airLevel + (treeLevel - 0.5f) * airSpeed * Time.deltaTime, 0, 1);
+        temperatureLevel = Mathf.Clamp(temperatureLevel + (airLevel - 0.5f) * tempSpeed * Time.deltaTime, 0, 1);
+
+        trees.fillAmount = treeLevel;
+        air.fillAmount = airLevel;
+        temperature.fillAmount = temperatureLevel;
+    }
+}
