6ddc9a61f85298fb6b5131a41abe0cc2f150186e
alanzhu07 <alanzhuyixuan@gmail.com>
2019-10-26 17:39:14 -0400

    add static play sfx method
    mixer.playSFX(string sfx)

--- Inhabitants/Assets/Scripts/mixer.cs
+++ Inhabitants/Assets/Scripts/mixer.cs
@@ -13,15 +13,22 @@ public class mixer : MonoBehaviour
     public int indCutoff;
     public int value = 95;
 
+
     public static void playSFX(string sfx)
     {
+        AudioSource fxSource = GameObject.Find("Sound/FX").GetComponent(typeof(AudioSource)) as AudioSource;
+        AudioClip clip = (AudioClip)Resources.Load<AudioClip>(sfx);
 
+        if (clip)
+            fxSource.PlayOneShot(clip);
+        else
+            Debug.LogError("no sfx associated with " + sfx);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        musicPlayer = GetComponent<AudioSource>();
+        //musicPlayer = GetComponent<AudioSource>();
         musicPlayer.clip = introMusic;
         musicPlayer.Play();
     }
