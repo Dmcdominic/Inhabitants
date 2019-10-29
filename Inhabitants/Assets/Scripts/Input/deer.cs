6022641d2acd44657a26710b19b4ea56dc0323f9
trsren <yumengren_999@outlook.com>
2019-10-28 21:44:51 -0400

    Added animal nodes (wolf&deer)

---
+++ Inhabitants/Assets/Scripts/Input/deer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deer : MonoBehaviour
+{
+
+    public float colony_num = 100f;
+    public float tree_effect = 1f;
+    private CircleCollider2D cc;
+    public float radius = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CircleCollider2D>();
+        this.gameObject.name = "deer";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //parameters
+        float tree_decrease_rate = -Time.deltaTime * colony_num * 0.0001f;
+        float colony_decrease_rate = 10f * Time.deltaTime;
+
+        //slow down growth of trees
+        cell_controller.instance.growTrees(transform.position, radius, tree_decrease_rate);
+
+        //if tree density is too small, gradually decrease # of deers
+        float deer_d = colony_num * Time.deltaTime * 10;
+        float tree_d = cell_controller.instance.tree_density(transform.position, radius);
+        if (deer_d > tree_d)
+        {
+            colony_num -= colony_decrease_rate;
+        }
+
+        //check if there are wolves nearby; if so, decrease size of colony 
+        Collider2D[] nearObjects = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (Collider2D obj in nearObjects)
+        {
+            if (obj != null && obj.name == "wolf")
+            {
+                colony_num -= colony_decrease_rate;
+
+            }
+
+
+        }
+        //if there are no deers left
+
+        if (colony_num <= 0 && this.gameObject != null)
+        {
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+}
---
+++ Inhabitants/Assets/Scripts/Input/wolf.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolf : MonoBehaviour
+{
+    public float colony_num = 50f;
+    private CircleCollider2D cc;
+    public float radius = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CircleCollider2D>();
+        this.gameObject.name = "wolf";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //parameters
+        float colony_decrease_rate = 10f * Time.deltaTime;
+        bool isDeer = false;
+
+        //checks if there are deers nearby
+        Collider2D[] nearObjects = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (Collider2D obj in nearObjects)
+        {   
+            if (obj!=null && obj.name == "deer")
+            {
+                isDeer = true;
+
+            }
+
+        }
+
+        //if there is no deer nearby, decrease colony size
+        if (this.gameObject!=null && !isDeer)
+        {
+            colony_num -= colony_decrease_rate;
+
+        }
+
+        //if there are no wolves left
+        if (colony_num <= 0 && this.gameObject != null)
+        {
+            Destroy(this.gameObject);
+            
+
+        }
+
+    }
+}
