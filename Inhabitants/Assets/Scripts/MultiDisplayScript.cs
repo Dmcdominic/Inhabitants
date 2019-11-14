feccebd6cff3c151a9184b3ec6cf1916b3b88b54
Joey Perrino <epiccrashv3@gmail.com>
2019-11-13 18:46:13 -0600

    Script handling multiple displays written
    - Script should activate all connected displays on startup
    - Connected to Human Camera

---
+++ Inhabitants/Assets/Scripts/MultiDisplayScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDisplayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // (Theoretically) activates all connected displays
+        // IMPORTANT! You cannot deactivate a display, and this must be called *once* during startup.
+        foreach (Display d in Display.displays)
+            d.Activate();
+    }
+}
